de9f537a53fe6178ef3f08530437cea91b20cba3
Anton Orlov <anton_orlov@ukr.net>
2023-01-06 09:36:52 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = document.querySelector('#ingredients');
+
+ingredients.forEach(element => {
+  const createEl = document.createElement('li');
+  itemEl.append(createEl);
+  createEl.textContent = element;
+  createEl.classList.add('item');
+});
+
+console.log(itemEl);
