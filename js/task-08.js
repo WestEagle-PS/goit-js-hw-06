f0d877937334e9238f127e88e8bd6520ba651b6e
Anton Orlov <anton_orlov@ukr.net>
2023-01-09 20:14:00 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const formEL = document.querySelector('.login-form');
 
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  const user = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(user);
+  event.currentTarget.reset();
+}
+
+formEL.addEventListener('submit', onFormSubmit);
