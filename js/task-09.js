ff4ebc2161890ec481fdb61af7d918f37f11055c
Anton Orlov <anton_orlov@ukr.net>
2023-01-09 20:54:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
+const buttonEl = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onButtonClick() {
+  let currentColor = getRandomHexColor();
+  bodyEl.style.backgroundColor = currentColor;
+  spanEl.textContent = currentColor;
+}
+
+buttonEl.addEventListener('click', onButtonClick);
